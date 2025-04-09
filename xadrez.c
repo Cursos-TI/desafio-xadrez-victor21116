6d947891bedb04bbbbfe1b1a250525c4b78b2cdc
victor21116 <victor21116@gmail.com>
2025-04-09 18:33:18 +0000

    usando recursividade e loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -1,41 +1,66 @@
 #include <stdio.h>
-int main(){
-int movimentoTorre = 5, movimentoBispo = 5, movimentoRainha = 8, movimentoCavalo = 1;
 
-    //variável de controle
-    int i;
-  
-
-    //Loop para movimentar a Torre 5 vezes para a Direita!
-    printf("Movimentando a torre!\n\n");
-
-    for (i = 1; i <= movimentoTorre; i++){
+void MovTorre (int MT){
+    if (MT > 0){
         printf("Direita!\n");
+        MovTorre(MT - 1);
     }
+}
+void MovBispo (int PassoAtual, int PassosMaximos){
+    if (PassoAtual> PassosMaximos) return;
 
-    //Loop para movimentar o bispo para diagonal direita
-    i = 1;
-    printf("\n\nMovimentando o bispo!\n\n");
-    while(i <= movimentoBispo){
-
-        printf("Cima, Direita!\n");
-        i++;
-    }
-    // Loop para movimentar a rainha
-    printf("\n\nMovimentando a Rainha!\n\n");
-    i = 1;
-    do{
-        printf("Esquerda\n");
-        i++;
-    }while(i <= movimentoRainha);
-    printf("\n\nMovimento Cavalo!\n\n");
-    
-
-    while(movimentoCavalo--){
-        for (i = 0 ; i < 2; i++){
-            printf("Cima!\n");
+    for (int  i = 1; i <= 1; i++){
+        printf("Cima!\n");
+        for (int j = 1; j <= i; j++){
+            printf("Direita!\n");
         }
-        printf("Direita\n");
     }
+
+    printf("\n");
+
+    MovBispo(PassoAtual +1 , PassosMaximos);
+        }
+
+void MovRainha (int MR){
+    if (MR > 0){
+        printf("Esquerda!\n");
+        MovRainha(MR - 1);
+    }
+}
+void MovCavalo (int m){
+    for (int i = 0; i < m; i++){
+        for ( int cima = 0; cima < 2; cima++){
+            printf("Cima!\n");
+            }
+            printf("Direita!\n");
+            printf("\n");
+        }
+    }
+       
+
+int main(){
+    //variáveis de controle
+int movimentoTorre = 5, movimentoBispo = 5, movimentoRainha = 8, MovimentoCavalo = 0;
+
+    //Chamada da função recursiva para movimentar a Torre 5 vezes para a Direita!
+    printf("\n\nMovimentando a torre!\n\n");
+    
+    MovTorre(movimentoTorre);
+    printf("\n\n");
+
+    //Chamada da Função recursiva para movimentar o bispo para diagonal direita
+    printf("\n\nMovimento bispo!\n\n");
+    MovBispo(1, movimentoBispo);
+
+    // Chmamada da função para movimentar a rainha
+    printf("\n\nMovimentando a Rainha!\n\n");
+    
+    MovRainha(movimentoRainha);
+
+    printf("\n\nMovimento Cavalo!\n\n");
+    printf("Quantos movimentos do cavalo vc quer fazer? ");
+    scanf("%d", &MovimentoCavalo);
+    
+    MovCavalo(MovimentoCavalo);
 
 }
